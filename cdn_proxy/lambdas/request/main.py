46d3227dac4fc3de188d79a8efe1adf927bd9896
Ryan Gerstenkorn <ryan.gerstenkorn@rhinosecuritylabs.com>
2021-08-21 21:09:47 -0700

    Add CloudFront scanner

--- cdn_proxy/lambdas/request/main.py
+++ cdn_proxy/lambdas/request/main.py
@@ -1,12 +1,18 @@
 import ipaddress
 import random
+import re
 
 import socket
+import json
 import struct
 
 # Lambda@Edge functions cannot have environment variables, so these get replaced at deploy time.
-HOST = None
+DEFAULT_HOST = None
 X_FORWARDED_FOR = None
+
+
+class CDNProxyError(Exception):
+    pass
 
 
 def random_ip():
@@ -18,28 +24,75 @@ def random_ip():
             return ip
 
 
+def hostname_for_ip(ip: str):
+    try:
+        host = socket.gethostbyaddr(ip)[0]
+        resolved_ip = socket.gethostbyname(host)
+        if ip == resolved_ip:
+            return host
+        else:
+            raise CDNProxyError('Unable to resolve the IP provided in Cdn-Proxy-Origin to a hostname. '
+                                'Forward and reverse resolved IPs did not match. As a workaround you can specify a'
+                                'hostname that resolves to this IP address.')
+    except socket.herror:
+        raise CDNProxyError('Unable to resolve the IP provided in Cdn-Proxy-Origin to a hostname. '
+                            'Reverse ptr resolution failed. As a workaround you can specify a'
+                            'hostname that resolves to this IP address.')
+    except socket.gaierror:
+        raise CDNProxyError('Unable to resolve the IP provided in Cdn-Proxy-Origin to a hostname. '
+                            'Forward resolution of the resulting reverse ptr failed. As a workaround you can specify a'
+                            'hostname that resolves to this IP address.')
+
+
 def lambda_handler(event, context):
+    print('event: ' + json.dumps(event))
     request = event['Records'][0]['cf']['request']
     headers = request['headers']
+    origin = request['origin']['custom']
 
-    if not HOST:
-        raise UserWarning('Something went wrong, the global variable HOST was not set during deploy.')
+    try:
+        request['headers'], request['origin']['custom'] = main(headers, origin)
+    except CDNProxyError as e:
+        return {
+            "body": ' '.join(e.args),
+            "bodyEncoding": 'text',
+            "headers": {},
+            "status": '400',
+            "statusDescription": 'Bad Request'
+        }
+
+    return request
+
+
+def main(headers, origin):
+    # Override default host if we find the Cdn-Proxy-Target header.
+    if len(headers.get('cdn-proxy-origin', [])) == 1:
+        value = headers['cdn-proxy-origin'][0]['value']
+        origin['domainName'] = value
+        if re.match(r'([0-9]{1,3}\.){3}[0-9]{1,3}', value):
+            origin['domainName'] = hostname_for_ip(value)
+
+    # Override the default origin if we get the Cdn-Proxy-Origin header.
+    host = DEFAULT_HOST
+    if len(headers.get('cdn-proxy-origin', [])) == 1:
+        host = headers['cdn-proxy-origin'][0]['value']
+    print(f'Setting Host to `{host}`.')
+    if not host:
+        raise UserWarning('Something went wrong, the global variable DEFAULT_HOST was not set during deploy and the '
+                          'request did not have the Cdn-Proxy-Host header set.')
+    headers['host'] = [{
+        "key": "Host",
+        "value": host,
+    }]
 
     if X_FORWARDED_FOR:
         forwarded_for = X_FORWARDED_FOR
     else:
         forwarded_for = random_ip()
-
-    print(f'Setting Host to `{HOST}`.')
-    headers['host'] = [{
-        "key": "Host",
-        "value": HOST,
-    }]
-
     print(f'Setting X-Forwarded-For to `{forwarded_for}`.')
+
     headers['x-forwarded-for'] = [{
         "key": "X-Forwarded-For",
         "value": forwarded_for,
     }]
-
-    return request
+    return headers, origin
--- cdn_proxy/lib.py
+++ cdn_proxy/lib.py
@@ -1,3 +1,7 @@
+import ipaddress
+from typing import List, Generator
+
+
 class CdnProxyException(Exception):
     pass
 
@@ -7,3 +11,9 @@ def trim(s: str, length: int):
         return s
     else:
         return s[0:length] + '...'
+
+
+def networks_to_hosts(networks: List[str]) -> Generator[str, None, None]:
+    for net in networks:
+        for host in ipaddress.ip_network(net).hosts():
+            yield host
