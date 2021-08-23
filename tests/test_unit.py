6fbe230f817dfa407b44d9571699d7c1b4dc60e7
Ryan Gerstenkorn <ryan.gerstenkorn@rhinosecuritylabs.com>
2021-08-22 18:20:03 -0700

    Fix testing

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -6,9 +6,10 @@ import boto3
 from moto import mock_iam, mock_lambda
 
 from cdn_proxy.cloudfront import CloudFront, CloudFrontProxy
+from cdn_proxy.lib import trim, networks_to_hosts
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def aws_credentials():
     """Mocked AWS Credentials for moto."""
     os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
@@ -17,13 +18,14 @@ def aws_credentials():
     os.environ['AWS_SESSION_TOKEN'] = 'testing'
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def sess(aws_credentials):
     with (mock_iam(), mock_lambda()):
         yield boto3.session.Session(region_name='us-east-1')
 
-@pytest.fixture
-def cloudfront(sess):
+
+@pytest.fixture(scope="module")
+def cloudfront(sess) -> 'CloudFront':
     CloudFront.status = Mock()
     CloudFront.status.return_value = CloudFrontProxy(
         id='test-id',
@@ -31,8 +33,46 @@ def cloudfront(sess):
     )
     return CloudFront(sess)
 
+# moto complains if you try to create a function using a role that doesn't exist, so these need to be tested together.
+@pytest.fixture
+def lambda_role_arn(cloudfront: 'CloudFront') -> 'CloudFront':
+    for msg in cloudfront.create_lambda_role():
+        print(msg)
+    return cloudfront.lambda_role_arn
+
+
+def test_lambda_role_arn(lambda_role_arn: str):
+    assert lambda_role_arn == 'arn:aws:iam::123456789012:role/cdn-proxy-request'
+
 
 # moto complains if you try to create a function using a role that doesn't exist, so these need to be tested together.
-def test_lib(cloudfront):
-    role_arn = cloudfront.create_lambda_role('test-function', 'lambda-cfn')
-    cloudfront.create_function('test-function', role_arn)
+@pytest.fixture
+def function(cloudfront: 'CloudFront', lambda_role_arn: 'str') -> 'CloudFront':
+    for msg in cloudfront.create_function(cloudfront.lambda_role_arn):
+        print(msg)
+
+
+def test_get_lambda_versions(cloudfront: 'CloudFront', function):
+    assert cloudfront.get_lambda_versions() == ["1"]
+
+
+def test_delete_lambda_role(cloudfront: 'CloudFront'):
+    cloudfront.delete_lambda_role()
+
+
+def test_delete_function(cloudfront: 'CloudFront', function):
+    cloudfront.delete_function()
+
+
+def test_trim():
+    assert trim("1234567890", 7) == "1234567..."
+    assert trim("123456", 7) == "123456"
+
+
+def test_networks_to_hosts():
+    assert list(networks_to_hosts(["192.168.0.1/32", "192.168.1.0/30"])) == [
+        "192.168.0.1",
+        "192.168.1.1",
+        "192.168.1.2",
+    ]
+
